8f9a15ef2be99a86894e754f3da30c6b58deff4f
allisonwinder <103597663+allisonwinder@users.noreply.github.com>
2023-02-23 15:20:49 -0700

    Beginning Controllers. pt 2

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -11,27 +11,73 @@ namespace Mission8_Group2.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private TaskContext dbContext { get; set; }
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(TaskContext someName)
         {
-            _logger = logger;
+            dbContext = someName;
         }
 
         public IActionResult Index()
         {
+            new tasks = dbContext.Responses
+                .Include(x => x.Categories)
+                .ToList();
+            return View(tasks);
+        }
+
+        [HttpGet]
+        public IActionResult TaskManager()
+        {
+            ViewBag.Categories = dbContext.Categories.ToList();
             return View();
         }
 
-        public IActionResult Privacy()
+        [HttpPost]
+        public IActionResult TaskManager(TaskResponse tr)
         {
-            return View();
+            if (ModelState.IsValid)
+            {
+                dbContext.Add(tr);
+                dbContext.SaveChanges();
+                return View("Index", tr);
+            }
+            else
+            {
+                ViewBag.Categories = dbContext.Categories.ToList();
+                return View(tr);
+            }
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        [HttpGet]
+        public IActionResult Edit(int taskid)
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            ViewBag.Categories = dbContext.Categories.ToList();
+            var task = dbContext.Responses.Single(x => x.TaskId == taskid);
+            return View("TaskManager", task);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(TaskResponse blah)
+        {
+            dbContext.Update(blah);
+            dbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int taskid)
+        {
+            var task = dbContext.Responses.Single(x => x.TaskId == taskid);
+            return View(task);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(TaskResponse tr)
+        {
+            dbContext.Responses.Remove(tr);
+            dbContext.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
