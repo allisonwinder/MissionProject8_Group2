4947fd5292651d71b8bc1dca1aae8b10313bec24
rylan-dunning <brdunning14@gmail.com>
2023-02-24 14:15:23 -0700

    added matrixinfocontext model and matrixresponse model, edited json, edited startup

---
+++ Models/MatrixResponse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission8_Group2.Models
+{
+    public class MatrixResponse
+    {
+        [Key]
+        [Required]
+        public int MatrixId { get; set; }
+
+        public string Task { get; set; }
+
+        public string DueDate { get; set; }
+
+        public int Quadrant { get; set; }
+
+        public string Category { get; set; }
+
+        public bool Completed { get; set; }
+
+    }
+}
